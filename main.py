2aa5415e28b8366abd994ce140fdbe4acb9d8810
MinertimeHD <80700363+MinertimeHD@users.noreply.github.com>
2021-12-25 10:50:16 +0100

    main.py refreshed, old and wrong version deleted

--- main.py
+++ main.py
@@ -1,84 +1,167 @@
-import datetime
-import sys
+import json
+import os
+
 from PyQt5 import QtCore, QtWidgets
 
 
-class Display(QtWidgets.QWidget):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("ToDo")
-        MainWindow.resize(800, 600)
-        MainWindow.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:0, y2:0, stop:0 rgba(255, 81, 177, 255), stop:1 rgba(137, 255, 28, 255));")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.todoList = QtWidgets.QListWidget(self.centralwidget)
-        self.todoList.setGeometry(QtCore.QRect(10, 140, 781, 451))
+class ToDoDisplay(QtWidgets.QWidget):
+    def setupUi(self, Form: QtWidgets.QWidget):
+        self.on = False
+        Form.setObjectName("Form")
+        Form.resize(1065, 691)
+        Form.setStyleSheet("background-color: rgb(79, 79, 79); border-radius: 10px;")
+        self.todoListLayoutWidget = QtWidgets.QWidget(Form)
+        self.todoListLayoutWidget.setGeometry(QtCore.QRect(110, 10, 461, 671))
+        self.todoListLayoutWidget.setObjectName("todoListLayoutWidget")
+        self.todoListLayout = QtWidgets.QHBoxLayout(self.todoListLayoutWidget)
+        self.todoListLayout.setContentsMargins(0, 0, 0, 0)
+        self.todoListLayout.setObjectName("todoListLayout")
+        self.todoList = QtWidgets.QListWidget(self.todoListLayoutWidget)
+        self.todoList.setStyleSheet("background-color: rgb(86, 86, 86); border-radius: 4px;")
         self.todoList.setObjectName("todoList")
-        self.plusBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.plusBtn.setGeometry(QtCore.QRect(30, 20, 51, 51))
-        self.plusBtn.setStyleSheet("background-image: url(plus.png);")
-        self.plusBtn.setObjectName("plusBtn")
-        self.plusBtn.clicked.connect(self.getAddInput)
-        self.minusBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.minusBtn.setGeometry(QtCore.QRect(100, 20, 51, 51))
-        self.minusBtn.setStyleSheet("background-image: url(cross.png);")
-        self.minusBtn.setObjectName("minusBtn")
-        self.minusBtn.clicked.connect(self.getRemInput)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.actionadd = QtWidgets.QAction(MainWindow)
-        self.actionadd.setObjectName("actionAdd")
-        self.actionrem = QtWidgets.QAction(MainWindow)
-        self.actionrem.setObjectName("actionRem")
+        self.todoListLayout.addWidget(self.todoList)
+        self.ToolBar = QtWidgets.QFrame(Form)
+        self.ToolBar.setGeometry(QtCore.QRect(10, 10, 81, 671))
+        self.ToolBar.setStyleSheet("background-color: rgb(86, 86, 86); border-radius: 30")
+        self.ToolBar.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.ToolBar.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.ToolBar.setObjectName("ToolBar")
+        self.addBtn = QtWidgets.QPushButton(self.ToolBar)
+        self.addBtn.setGeometry(QtCore.QRect(10, 10, 61, 61))
+        self.addBtn.setStyleSheet("border-radius: 30px;\n"
+                                  "background-color: rgb(172, 172, 172);")
+        self.addBtn.setText("Add")
+        self.addBtn.setObjectName("addBtn")
+        self.removeBtn = QtWidgets.QPushButton(self.ToolBar)
+        self.removeBtn.setGeometry(QtCore.QRect(10, 80, 61, 61))
+        self.removeBtn.setStyleSheet("border-radius: 30px;\n"
+                                     "background-color: rgb(172, 172, 172);\n"
+                                     "border: 10px;\n"
+                                     "border-color: rgb(94, 94, 94);")
+        self.removeBtn.setText("Remove")
+        self.removeBtn.setObjectName("removeBtn")
+        # self.saveBtn = QtWidgets.QPushButton(self.ToolBar)
+        # self.saveBtn.setGeometry(QtCore.QRect(10, 540, 61, 61))
+        # self.saveBtn.setStyleSheet("border-radius: 30px;\n"
+        #                            "background-color: rgb(172, 172, 172);\n"
+        #                            "border: 1px;")
+        # self.saveBtn.setText("Save")
+        # self.saveBtn.setObjectName("saveBtn")
+        self.resetBtn = QtWidgets.QPushButton(self.ToolBar)
+        self.resetBtn.setGeometry(QtCore.QRect(10, 600, 61, 61))
+        self.resetBtn.setStyleSheet("border-radius: 30px;\n"
+                                    "border: 1px;\n"
+                                    "background-color: rgb(172, 172, 172);")
+        self.resetBtn.setText("Reset")
+        self.resetBtn.setObjectName("resetBtn")
+        self.calendarLayoutWidget = QtWidgets.QWidget(Form)
+        self.calendarLayoutWidget.setGeometry(QtCore.QRect(593, 10, 461, 671))
+        self.calendarLayoutWidget.setObjectName("calendarLayoutWidget")
+        self.calendarLayout = QtWidgets.QHBoxLayout(self.calendarLayoutWidget)
+        self.calendarLayout.setContentsMargins(0, 0, 0, 0)
+        self.calendarLayout.setObjectName("calendarLayout")
+        self.calendar = QtWidgets.QCalendarWidget(self.calendarLayoutWidget)
+        self.calendar.setStyleSheet("QMenu {\n"
+                                    "\n"
+                                    "background-color: rgb(86, 86, 86)\n"
+                                    "\n"
+                                    "}\n"
+                                    "QToolButton {\n"
+                                    "\n"
+                                    "background-color: rgb(99, 99, 99)\n"
+                                    "\n"
+                                    "}\n"
+                                    "QAbstractItemView {selection-background-color: rgb(85, 88, 92); background-color: rgb(86,86,86); selection-color: rgb(0,255,0)}\n"
+                                    "QWidget {background-color: rgb(86, 86, 86); \n"
+                                    "alternate-background-color:rgb(100,100,100)};")
+        self.calendar.setGridVisible(True)
+        self.calendar.setHorizontalHeaderFormat(QtWidgets.QCalendarWidget.ShortDayNames)
+        self.calendar.setVerticalHeaderFormat(QtWidgets.QCalendarWidget.NoVerticalHeader)
+        self.calendar.setNavigationBarVisible(True)
+        self.calendar.setDateEditEnabled(True)
+        self.calendar.setObjectName("calendar")
+        self.calendar.selectionChanged.connect(lambda: self.loadTasks(True))
+        self.calendarLayout.addWidget(self.calendar)
+        self.addBtn.clicked.connect(self.addTask)
+        self.removeBtn.clicked.connect(self.removeTask)
+        # self.saveBtn.clicked.connect(self.save)
+        self.resetBtn.clicked.connect(self.resetTasks)
+        self.loadTasks()
 
-        self.plusBtn.setStyleSheet("background-image: url(plus.png);\nbackground-color: rgb(199, 199, 199);")
-        self.minusBtn.setStyleSheet("background-image: url(cross.png);\nbackground-color: rgb(199, 199, 199);")
-        self.todoList.setStyleSheet("background-color: rgb(85, 255, 255);\nfont: 87 11pt \"Arial Black\";")
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+        Form.setFixedSize(Form.width(), Form.height())
+        Form.setWindowTitle("PyCalendar - by Fallcrim")
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    def retranslateUi(self, Form):
+        pass
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        current_time = datetime.datetime.now().strftime("%d.%m.%Y")
-        MainWindow.setWindowTitle(_translate("MainWindow", "ToDo - " + str(current_time)))
-
-    def getAddInput(self):
-        text, ok = QtWidgets.QInputDialog.getText(self, 'Input Dialog', 'Enter new task:')
-
-        if ok:
-            self.newTask(text)
-
-    def getRemInput(self):
-        text, ok = QtWidgets.QInputDialog.getText(self, 'Input Dialog', 'Enter task to remove:')
-
-        if ok:
-            self.removeTask(text)
-
-    def newTask(self, task: str):
-        self.todoList.addItem(task)
-        dialog = QtWidgets.QDialog(self)
-        if dialog:
-            return
-
-    def removeTask(self, name: str):
-        items = []
+    def getAllTasks(self):
+        tasks = []
         for i in range(self.todoList.count()):
-            items.append(self.todoList.item(i))
-        for item in items:
-            if item.text() == name:
-                index = items.index(item)
-                self.todoList.takeItem(index)
-        dialog = QtWidgets.QDialog(self)
-        if dialog:
-            return
+            tasks.append(self.todoList.item(i).text())
+        return tasks
+
+    def addTask(self):
+        text, ok = QtWidgets.QInputDialog.getText(self, 'New task', 'Enter new task:')
+
+        if ok:
+            self.todoList.addItem(text)
+            self.save()
+
+    def save(self):
+        if not os.path.isfile("tasks.json"):
+            with open("tasks.json", "w") as new:
+                new.write("{}")
+        with open("tasks.json", "r") as f:
+            current_tasks = json.load(f)
+        current_tasks[self.calendar.selectedDate().toString()] = self.getAllTasks()
+        with open("tasks.json", "w") as db:
+            json.dump(current_tasks, db)
+        date_string = self.calendar.selectedDate().toString()
+        print(f"Tasks of day {date_string} saved.")
+
+    def removeTask(self):
+        self.todoList.takeItem(self.todoList.currentRow())
+        self.save()
+
+    def resetTasks(self, d=True):
+        if d:
+            diag = QtWidgets.QMessageBox(self)
+            diag.question(self, '', "Are you sure to reset all the values?", diag.Yes | diag.No)
+
+            if diag == diag.Yes:
+                self.todoList.clear()
+            else:
+                return
+        else:
+            self.todoList.clear()
+
+    def loadTasks(self, reset=False):
+        if reset:
+            self.resetTasks(d=False)
+        try:
+            with open("tasks.json") as db:
+                tasks = json.load(db)
+            if self.calendar.selectedDate().toString() in tasks.keys():
+                days_tasks = tasks[self.calendar.selectedDate().toString()]
+            else:
+                days_tasks = []
+            for task in days_tasks:
+                self.todoList.addItem(task)
+        except FileNotFoundError:
+            f = open("tasks.json", "w")
+            f.write("{}")
+            f.close()
+            self.loadTasks()
 
 
+if __name__ == "__main__":
+    import sys
 
-
-app = QtWidgets.QApplication(sys.argv)
-MainWindow = QtWidgets.QMainWindow()
-ui = Display()
-ui.setupUi(MainWindow)
-
-
-MainWindow.show()
-sys.exit(app.exec_())
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = ToDoDisplay()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
